063e7df6a03f625aec047ed637df4abca6da3cb2
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2020-03-26 19:13:58 +0100

    fixed behaviour such that pacman only gets the pwoeup when the pellet is actually eaten, split the normal and powerpellets such that thy both use different scripts now

---
+++ Assets/Scripts/pacDotPowerPelletScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class pacDotPowerPelletScript : MonoBehaviour
+{
+    public PacManMoveScript pacMan;
+
+    private Vector3[] locations = {
+        new Vector3(22, 11, 0),
+        new Vector3(10, 11, 0),
+        new Vector3(19, 23, 0),
+        new Vector3(10, 23, 0),
+        new Vector3(10, 11, 0),
+        new Vector3(19, 11, 0)
+    };
+    
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "PacMan")
+        {
+            if(pacMan.powerPelletEaten){ 
+                Destroy(gameObject);
+            }
+            else{
+                int randomIndex = Random.Range(0,7);
+                Vector3 newPosition = locations[randomIndex];
+                transform.position = newPosition;
+            }
+        }
+    }
+}
